0bbf76dacbb59783fc292cc4e6ccea4038b55a55
Rajiv McCoy <rajiv.mccoy@jhu.edu>
2020-12-19 13:47:11 -0500

    combine all mixcr steps with "analyze"

--- mixcr.sh
+++ mixcr.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=mixcr
-#SBATCH --time=36:0:0
+#SBATCH --time=48:0:0
 #SBATCH --partition=shared
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=12
-#SBATCH--array=0-231%24
+#SBATCH--array=1-231%24
 
 ml java
 ml samtools
@@ -15,24 +15,23 @@ cd /scratch/users/rmccoy22@jhu.edu/mixcr
 cram_path=`sed "${SLURM_ARRAY_TASK_ID}q;d" sample_list_paths.txt`
 sample_id=`basename ${cram_path} .cram`
 
+if [ ! -f ${sample_id}.R1.fq.gz ]; then
 samtools fastq \
+-@ 12 \
+-c 6 \
 --reference /work-zfs/rmccoy22/resources/reference/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna \
--@12 \
--1 ${sample_id}.R1.fq \
--2 ${sample_id}.R2.fq \
+-1 ${sample_id}.R1.fq.gz \
+-2 ${sample_id}.R2.fq.gz \
 ${cram_path}
+fi
 
-bgzip -@ 12 ${sample_id}.R1.fq
-bgzip -@ 12 ${sample_id}.R2.fq
-
-~/code/mixcr-3.0.13/mixcr align \
--f \
--OvParameters.geneFeatureToAlign=VGeneWithP \
+~/code/mixcr-3.0.13/mixcr analyze shotgun \
 -s hsa \
+--starting-material dna \
 --report ${sample_id}_analysis.report \
--t 12 \
+--align "-t 12" \
 ${sample_id}.R1.fq.gz ${sample_id}.R2.fq.gz \
-~/scratch/${sample_id}_alignments.vdjca
+${sample_id}_alignments.vdjca
 
 rm ${sample_id}.R1.fq.gz
 rm ${sample_id}.R2.fq.gz
